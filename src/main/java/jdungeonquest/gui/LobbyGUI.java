cebf2a2303c4273ae83cbaf76913e6b42f4c095f
gman-x <gothicmanx@gmail.com>
2013-04-27 22:44:19 +0700

    LobbyGUI WIP

--- src/main/java/jdungeonquest/gui/LobbyGUI.java
+++ src/main/java/jdungeonquest/gui/LobbyGUI.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import jdungeonquest.network.NetworkClient;
 import net.miginfocom.swing.MigLayout;
 
@@ -18,11 +19,13 @@ class LobbyGUI extends JPanel{
     
     NetworkClient client;
     //UserData data;
-    
-    MigLayout layout;
     //ChatBox chat;
     //GameMap map;
     //JTextArea textArea;
+    JList messageList;
+    JList playerList;
+    ConnectPanel connectPanel;
+    JTextField textField;
     
     LobbyGUI(GUI parent, NetworkClient client){
         this.parent = parent;
@@ -32,27 +35,76 @@ class LobbyGUI extends JPanel{
     }
 
     private void initGUI() {
-        layout = new MigLayout("fill", "[][]", "[fill, grow][fill, grow]");
+        MigLayout layout = new MigLayout("fill", "[][]", "[][]");
         this.setLayout(layout);
 
-        JLabel clientTest = new JLabel("client test");
+        connectPanel = new ConnectPanel(this);
+        textField = new JTextField("");
+
         JButton goBackButton = new JButton("Back");
+        JButton sendButton = new JButton("Send");
         goBackButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 parent.showMainMenu();
             }
         });
-        add(clientTest);
-        add(goBackButton, "grow, wrap");
         
         String time = new SimpleDateFormat("[HH:mm:ss]").format(new Date());
-        JList messageList;
-        add(new JScrollPane(messageList = new JList()), "grow, span");
+        messageList = new JList();
         messageList.setModel(new DefaultListModel());
-        ((DefaultListModel)messageList.getModel()).addElement(time + ": " + "test message");
-        messageList.ensureIndexIsVisible(((DefaultListModel)messageList.getModel()).size() - 1);
-
+        messageList.ensureIndexIsVisible(((DefaultListModel) messageList.getModel()).size() - 1);
         
+        add(connectPanel, "growx, span, wrap");
+        add(textField, "growx, push");
+        add(sendButton, "w 70!, wrap");
+        add(new JScrollPane(messageList), "grow, span");
+        add(goBackButton, "growx, span");
+    }
+
+    private static class ConnectPanel extends JPanel {
+
+        LobbyGUI parent;
+        JTextField nameTextField;
+        JTextField portTextField;
+        JTextField ipTextField;
+
+        public ConnectPanel(final LobbyGUI p) {
+            this.parent = p;
+
+            MigLayout layout = new MigLayout("fill", "[][]", "[][]");
+            setLayout(layout);
+
+            JLabel ipLabel = new JLabel("Server address:");
+            ipTextField = new JTextField("127.0.0.1");
+
+            JLabel portLabel = new JLabel("Server port:");
+            portTextField = new JTextField("4445");
+
+            JLabel nameLabel = new JLabel("Player name:");
+            nameTextField = new JTextField("GenericPlayer");
+
+            JLabel infoLabel = new JLabel("Not connected to the server");
+
+            JButton connectButton = new JButton("Connect");
+            connectButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(parent.client != null){
+                        parent.client.stop();
+                    }
+                    parent.client = new NetworkClient(nameTextField.getText(), ipTextField.getText(), Integer.parseInt(portTextField.getText()));
+                    parent.client.run();
+                    parent.client.registerOnServer();
+                }
+            });
+
+            add(infoLabel, "grow, span");
+            add(ipLabel);
+            add(ipTextField, "wrap");
+            add(portLabel);
+            add(portTextField, "wrap");
+            add(connectButton, "grow, span");
+        }
     }
 }
