d13d673fd1fe9fa32a29b70dff3f32da72dcf305
gman-x <gothicmanx@gmail.com>
2013-04-29 18:51:31 +0700

    Added ChatMessage class

---
+++ src/main/java/jdungeonquest/network/ChatMessage.java
@@ -0,0 +1,14 @@
+package jdungeonquest.network;
+
+import jdungeonquest.enums.NetworkMessageType;
+
+public class ChatMessage extends Message{
+    String message;
+    String author;
+
+    ChatMessage(String text, String clientName) {
+        msgType = NetworkMessageType.ChatMessage;
+        message = text;
+        author = clientName;
+    }
+}
