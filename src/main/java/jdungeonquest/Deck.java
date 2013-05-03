003099ab862b3477fbc6fd988430fd3a12de8906
gman-x <gothicmanx@gmail.com>
2013-05-03 14:06:46 +0700

    Made a barebone Deck class

--- src/main/java/jdungeonquest/Deck.java
+++ src/main/java/jdungeonquest/Deck.java
@@ -1,5 +1,27 @@
 package jdungeonquest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+ 
+    List<Card> cardArray = new ArrayList<>();
     
+    public Card takeCard(){
+        Card c = cardArray.get(0);
+        cardArray.remove(0);
+        return c;
+    }
+    
+    public void shuffle(){
+        
+    }
+    
+    public int size(){
+        return cardArray.size();
+    }
+
+    void putCard(Card card) {
+        cardArray.add(card);
+    }
 }
