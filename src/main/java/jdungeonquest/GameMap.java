400f101c8e6fc9c852ba54f60c88b8e32de8da5d
gman-x <gothicmanx@gmail.com>
2013-05-04 21:43:23 +0700

    Added GameMap class

---
+++ src/main/java/jdungeonquest/GameMap.java
@@ -0,0 +1,41 @@
+package jdungeonquest;
+
+public class GameMap {
+    public final static int MAX_X = 10;
+    public final static int MAX_Y = 13;
+
+    private Tile[][] tiles =  new Tile[10][13];
+    
+    public GameMap(){
+        fillWithDummyTiles();
+    }
+   
+    public void setTile(int x, int y, Tile tile) {
+        tiles[x][y] = tile;
+    }
+
+    public Tile getTile(int x, int y) {
+        return tiles[x][y];
+    }
+
+    public Tile[][] getTiles(){
+        return tiles;
+    }
+    
+    private void fillWithDummyTiles() {
+        for (int x = 0; x < GameMap.MAX_X; x++) {
+            for (int y = 0; y < GameMap.MAX_Y; y++) {
+                setTile(x, y, new Tile());
+            }
+        }
+    }
+    
+    public boolean isAdjacent(int x1, int y1, int x2, int y2){
+        return false;        
+    }
+
+    public boolean canMoveTo(int x1, int y1, int x2, int y2){
+        return false;
+    }
+    
+}
