3da6bfb6f3e34fb53f86c973200398633a387429
gman-x <gothicmanx@gmail.com>
2013-05-18 04:26:49 +0700

    Clarified SecretDoor message

--- src/main/java/jdungeonquest/effects/SecretDoor.java
+++ src/main/java/jdungeonquest/effects/SecretDoor.java
@@ -7,7 +7,7 @@ public class SecretDoor implements Effect{
 
     @Override
     public void doAction(Game g) {
-        g.addMessage(new ChatMessage(g.getCurrentPlayer() + " has found a secret door!", "Game"));
+        g.addMessage(new ChatMessage(g.getCurrentPlayer() + " has found a secret door! You can move in any direction.", "Game"));
         g.processSecretDoor();
     }
 }
